e57fbba85c83eb74e8ff18ea49d25c6e506a19fb
kamaDe25 <kamalikade25@gmail.com>
2026-05-10 12:58:22 -0700

    getting started on level2

--- script.js
+++ script.js
@@ -116,7 +116,6 @@ class Level1 extends Phaser.Scene{
         this.tile = this.load.image('tile', 'assets/MapSymbols/Square1x1-Lowres.png')
         this.bowl = this.load.image('bowl', 'assets/Pixel_Mart/bowl.png')
         this.load.spritesheet('mouse', 'assets/SmallAnimals/Mouse.png', {frameWidth: 16, frameHeight: 16})
-        //this.slope = this.load.image('slope', 'assets/MapSymbols/TriangleArrowhead1x1-Lowres.png')
         this.button = this.load.image('button', 'assets/MapSymbols/DoorFalse1x1-Lowres.png')
         this.fan = this.load.image('fan', 'assets/MapSymbols/PitClosedCircle1x1-Lowres.png')
         this.load.image('cheese', 'assets/Pixel_Mart/white_cheese_piece.png')
@@ -133,15 +132,18 @@ class Level1 extends Phaser.Scene{
         //adding stuff
         this.mouse = this.physics.add.sprite(50, 500, 'mouse').setFlipX(true).setScale(3).setCollideWorldBounds(true, 0, 0);
         this.bowl = this.physics.add.staticImage(700, 550, 'bowl').setScale(3)
+        //this.bowl.body.setSize(1)
+        //this.bowl.refreshBody()
         this.fan = this.physics.add.image(690, 400, 'fan').setAngularDrag(0).setAngularVelocity(360)
         this.fan.body.setAllowGravity(false)
         this.button = this.physics.add.staticImage(230, 490, 'button').setScale(0.4)
-        this.cheese = this.physics.add.image(680, 250, 'cheese')
+        this.cheese = this.physics.add.image(685, 250, 'cheese')
 
         //make sure stuff is solid
         this.physics.add.collider(this.mouse, this.platforms);
-        this.physics.add.collider(this.cheese, this.bowl);
+        //this.physics.add.collider(this.cheese, this.bowl);
         this.physics.add.overlap(this.mouse, this.button);
+        this.physics.add.collider(this.mouse, this.bowl);
 
         this.cursors = this.input.keyboard.createCursorKeys();
 
@@ -221,9 +223,108 @@ class Level2 extends Phaser.Scene{
         super('level2');
     }
 
-    preload(){}
-    create(){}
-    update(){}
+    preload(){
+        this.tile = this.load.image('tile', 'assets/MapSymbols/Square1x1-Lowres.png')
+        this.bowl = this.load.image('bowl', 'assets/Pixel_Mart/bowl.png')
+        this.load.spritesheet('mouse', 'assets/SmallAnimals/Mouse.png', {frameWidth: 16, frameHeight: 16})
+        this.button = this.load.image('button', 'assets/MapSymbols/DoorFalse1x1-Lowres.png')
+        this.fan = this.load.image('fan', 'assets/MapSymbols/PitClosedCircle1x1-Lowres.png')
+        this.load.image('cheese', 'assets/Pixel_Mart/white_cheese_piece.png')
+    }
+    create(){
+         //build environment
+        this.platforms = this.physics.add.staticGroup();
+        let i = 0;
+        for(i = 0; i < tilePositions.length; i++){
+            this.platforms.create(tilePositions[i].x, tilePositions[i].y, 'tile').setScale(0.5).refreshBody();
+        }
+
+        //adding stuff
+        this.mouse = this.physics.add.sprite(50, 500, 'mouse').setFlipX(true).setScale(3).setCollideWorldBounds(true, 0, 0);
+        this.bowl = this.physics.add.staticImage(700, 550, 'bowl').setScale(3)
+        //this.bowl.body.setSize(1)
+        //this.bowl.refreshBody()
+        this.fan = this.physics.add.image(690, 400, 'fan').setAngularDrag(0).setAngularVelocity(360)
+        this.fan.body.setAllowGravity(false)
+        this.button = this.physics.add.staticImage(230, 490, 'button').setScale(0.4)
+        this.cheese = this.physics.add.image(685, 250, 'cheese')
+
+        //make sure stuff is solid
+        this.physics.add.collider(this.mouse, this.platforms);
+        //this.physics.add.collider(this.cheese, this.bowl);
+        this.physics.add.overlap(this.mouse, this.button);
+        this.physics.add.collider(this.mouse, this.bowl);
+
+        this.cursors = this.input.keyboard.createCursorKeys();
+
+        this.fanOn = true;
+        //this.mouse.body.onCollide = true
+        this.physics.add.overlap(
+            this.mouse,
+            this.button,
+            () => {
+                if(this.fanOn == true){
+                    this.fanOn = false
+                }else{
+                    this.fanOn = true
+                }
+            }
+        )
+
+        this.physics.add.overlap(
+            this.cheese,
+            this.bowl,
+            () => {
+                console.log('overlap detected')
+                this.textObjectKama = this.add.text(
+                    250,     // x
+                    0,    // y
+                    "You Win!",
+                    { font: "75px Arial", color: "#f9f7f5" }
+                );
+                this.time.delayedCall(
+                    2000,
+                    () => {  this.scene.start('level2');}
+                );
+            }
+        )    
+    }
+    update(){
+        //player movement
+        const { left, right, up } = this.cursors;
+
+        if (left.isDown)
+        {
+            this.mouse.setVelocityX(-160);
+
+            //this.mouse.anims.play('left', true);
+        }
+        else if (right.isDown)
+        {
+            this.mouse.setVelocityX(160);
+
+            //this.mouse.anims.play('right', true);
+        }
+        else
+        {
+            this.mouse.setVelocityX(0);
+
+            //this.mouse.anims.play('turn');
+        }
+        if (up.isDown && this.mouse.body.blocked.down){
+            this.mouse.setVelocityY(-250);
+        }
+
+        if(this.fanOn == true){
+            this.fan.setAngularAcceleration(0).setAngularDrag(0);
+            //rotate fan
+            //set cheese y velocity to negative so it looks like its being held up by cheese
+            this.cheese.body.setAllowGravity(false);
+        }else{
+            this.cheese.body.setAllowGravity(true);
+            //set cheese y velocity to 0
+        }
+    }
 }
 //configuration stuff below ---------------------------------------------------------------------------
 let config = {
@@ -235,7 +336,7 @@ let config = {
     physics: {
         default: 'arcade',
         arcade: {
-            debug: false,
+            debug: true,
             gravity: {y: 200}
         }
     },
